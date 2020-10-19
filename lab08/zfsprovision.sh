f9fe5493a8544466eb2f6919a0d14731565806e3
Deron-D <deron73@inbox.ru>
2020-10-19 21:16:21 +0300

    Домашнее задание №8: ZFS

--- lab08/zfsprovision.sh
+++ lab08/zfsprovision.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
-yum install -y yum-utils
+yum install -y yum-utils wget
 yum -y install http://download.zfsonlinux.org/epel/zfs-release.el7_4.noarch.rpm
 gpg --quiet --with-fingerprint /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
 yum-config-manager --enable zfs-kmod
@@ -10,3 +10,4 @@ yum-config-manager --disable zfs
 yum repolist | grep zfs && echo ZFS repo enabled
 yum install -y zfs
 /sbin/modprobe zfs
+chmod +x /vagrant/download_gdrive
