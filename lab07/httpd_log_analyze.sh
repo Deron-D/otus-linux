7953b1b04964eab6c44396d0befb6516565230dc
Deron-D <deron73@inbox.ru>
2020-10-20 22:08:29 +0300

    Домашнее задание №7: Bash

--- lab07/httpd_log_analyze.sh
+++ lab07/httpd_log_analyze.sh
@@ -15,14 +15,26 @@ EMAIL=root@localhost
 
 #Main function
 analyze_log_file() {
-	INPUTFILE=$1
-	OUTPUTFILE=$2
-	BEGINTIME=`head -n 1 $1 |awk '{print $4}'`
-	ENDTIME=`tail -n 1 $1 |awk '{print $4}'`
-	echo $BEGINTIME; echo $ENDTIME
+	INPUTFILE=$1; OUTPUTFILE=$2
+	BEGINTIME=`head -n 1 $1 |awk '{print $4}'| cut -c2-`; ENDTIME=`tail -n 1 $1 |awk '{print $4}' | cut -c2-`
+	echo "=============================================" > $OUTPUTFILE
+	echo "HTTPD usage report                           " >> $OUTPUTFILE
+	echo "Analyze period is from $BEGINTIME to $ENDTIME" >> $OUTPUTFILE
+	echo "=============================================" >> $OUTPUTFILE
+	echo "$X IP addresses (with the largest number of requests)" >> $OUTPUTFILE 
+        cat $1 |awk '{print $1}' |sort |uniq -c |sort -rn| tail -$X >> $OUTPUTFILE
+        echo "---------------------------------------------" >> $OUTPUTFILE
+        echo "$Y requested addresses (with the largest number of requests)" >> $OUTPUTFILE
+        cat $1 |awk '{print $7}' |sort |uniq -c |sort -rn| tail -$Y >> $2
+        echo "---------------------------------------------" >> $OUTPUTFILE
+        echo "All errors since the last launch" >> $OUTPUTFILE
+        cat $1 |awk '{print $9}' |grep -E "[4-5]{1}[0-9][0-9]" |sort |uniq -c |sort -rn >> $OUTPUTFILE
+        echo "---------------------------------------------" >> $OUTPUTFILE
+        echo "A list of all return codes indicating their number since the last launch" >> $OUTPUTFILE
+        cat $1 |awk '{print $9}' |sort |uniq -c |sort -rn >> $OUTPUTFILE
+	echo "---------------------------------------------" >> $OUTPUTFILE
+
 }
-
-
 
 
 if ( set -C; echo "$$" > "$LOCKFILE" ) 2> /dev/null; 
