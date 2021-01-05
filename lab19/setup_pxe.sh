3ff894005b1bdbd44ce1badada520ad5ee7c9d30
Deron-D <69377214+Deron-D@users.noreply.github.com>
2021-01-05 17:27:40 +0300

    Домашнее задание №19: Настройка PXE сервера для автоматической установки

--- lab19/setup_pxe.sh
+++ lab19/setup_pxe.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 echo Install PXE server
@@ -6,7 +5,7 @@ yum -y install epel-release
 
 yum -y install dhcp-server
 yum -y install tftp-server
-yum -y install nfs-utils
+#yum -y install nfs-utils
 firewall-cmd --add-service=tftp
 # disable selinux or permissive
 setenforce 0
@@ -19,15 +18,12 @@ option pxelinux.configfile code 209 = text;
 option pxelinux.pathprefix code 210 = text;
 option pxelinux.reboottime code 211 = unsigned integer 32;
 option architecture-type code 93 = unsigned integer 16;
-
 subnet 10.0.0.0 netmask 255.255.255.0 {
 	#option routers 10.0.0.254;
 	range 10.0.0.100 10.0.0.120;
-
 	class "pxeclients" {
 	  match if substring (option vendor-class-identifier, 0, 9) = "PXEClient";
 	  next-server 10.0.0.20;
-
 	  if option architecture-type = 00:07 {
 	    filename "uefi/shim.efi";
 	    } else {
@@ -54,18 +50,16 @@ cat >/var/lib/tftpboot/pxelinux/pxelinux.cfg/default <<EOF
 default menu
 prompt 0
 timeout 600
-
 MENU TITLE Demo PXE setup
-
-LABEL linux
-  menu label ^Install system
+LABEL linux-http-install
+  menu label ^Install system from http
   menu default
-  kernel images/CentOS-8.2/vmlinuz
-  append initrd=images/CentOS-8.2/initrd.img ip=enp0s3:dhcp inst.repo=nfs:10.0.0.20:/mnt/centos8-install
+  kernel images/CentOS-8/vmlinuz
+  append initrd=images/CentOS-8/initrd.img ip=enp0s3:dhcp inst.repo=http://10.0.0.20/centos8-install/ inst.stage2=http://10.0.0.20/install.img
 LABEL linux-auto
   menu label ^Auto install system
-  kernel images/CentOS-8.2/vmlinuz
-  append initrd=images/CentOS-8.2/initrd.img ip=enp0s3:dhcp inst.ks=nfs:10.0.0.20:/home/vagrant/cfg/ks.cfg inst.repo=nfs:10.0.0.20:/mnt/centos8-autoinstall
+  kernel images/CentOS-8/vmlinuz
+  append initrd=images/CentOS-8/initrd.img ip=enp0s3:dhcp inst.ks=nfs:10.0.0.20:/home/vagrant/cfg/ks.cfg inst.repo=http://10.0.0.20/centos8-autoinstall
 LABEL vesa
   menu label Install system with ^basic video driver
   kernel images/CentOS-8.2/vmlinuz
@@ -79,21 +73,34 @@ LABEL local
   localboot 0xffff
 EOF
 
-mkdir -p /var/lib/tftpboot/pxelinux/images/CentOS-8.2/
-curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/pxeboot/initrd.img
-curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/pxeboot/vmlinuz
-cp {vmlinuz,initrd.img} /var/lib/tftpboot/pxelinux/images/CentOS-8.2/
+mkdir -p /var/lib/tftpboot/pxelinux/images/CentOS-8/
+curl http://mirror.yandex.ru/centos/8/BaseOS/x86_64/os/images/pxeboot/initrd.img -o /var/lib/tftpboot/pxelinux/images/CentOS-8/initrd.img
+curl http://mirror.yandex.ru/centos/8/BaseOS/x86_64/os/images/pxeboot/vmlinuz -o /var/lib/tftpboot/pxelinux/images/CentOS-8/vmlinuz
+#cp {vmlinuz,initrd.img} /var/lib/tftpboot/pxelinux/images/CentOS-8/
 
 
-# Setup NFS auto install
+# Setup HTTP auto install
 # 
 
-curl -O http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/images/boot.iso
+#curl -O http://mirror.yandex.ru/centos/8.3.2011/BaseOS/x86_64/os/images/boot.iso
+curl -O http://mirror.yandex.ru/centos/8/BaseOS/x86_64/os/images/boot.iso
 mkdir /mnt/centos8-install
-mount -t iso9660 boot.iso /mnt/centos8-install
-echo '/mnt/centos8-install *(ro)' > /etc/exports
-systemctl start nfs-server.service
+#systemctl start nfs-server.service
+yum install httpd -y
+cat > /etc/httpd/conf.d/pxeboot.conf <<EOF_pxeboot_conf
+# create new
+Alias /centos8-install /mnt/centos8-install
+<Directory /mnt/centos8-install>
+    Options Indexes FollowSymLinks
+    # access permission
+    Require all granted
+</Directory>
+ 
+EOF_pxeboot_conf
 
+curl http://mirror.yandex.ru/centos/8/BaseOS/x86_64/os/images/install.img -o /var/www/html/install.img
+
+systemctl restart httpd
 
 autoinstall(){
   # to speedup replace URL with closest mirror
@@ -116,7 +123,6 @@ keyboard --vckeymap=us --xlayouts='us'
 lang en_US.UTF-8
 #repo
 #url --url=http://ftp.mgts.by/pub/CentOS/8.2.2004/BaseOS/x86_64/os/
-
 # Network information
 network  --bootproto=dhcp --device=enp0s3 --ipv6=auto --activate
 network  --bootproto=dhcp --device=enp0s8 --onboot=off --ipv6=auto --activate
@@ -132,23 +138,17 @@ services --enabled="chronyd"
 # System timezone
 timezone America/New_York --isUtc
 user --groups=wheel --name=val --password=$6$ihX1bMEoO3TxaCiL$OBDSCuY.EpqPmkFmMPVvI3JZlCVRfC4Nw6oUoPG0RGuq2g5BjQBKNboPjM44.0lJGBc7OdWlL17B3qzgHX2v// --iscrypted --gecos="val"
-
 %packages
 @^minimal-environment
 kexec-tools
-
 %end
-
 %addon com_redhat_kdump --enable --reserve-mb='auto'
-
 %end
-
 %anaconda
 pwpolicy root --minlen=6 --minquality=1 --notstrict --nochanges --notempty
 pwpolicy user --minlen=6 --minquality=1 --notstrict --nochanges --emptyok
 pwpolicy luks --minlen=6 --minquality=1 --notstrict --nochanges --notempty
 %end
-
 EOF
 echo '/home/vagrant/cfg *(ro)' >> /etc/exports
   systemctl reload nfs-server.service
