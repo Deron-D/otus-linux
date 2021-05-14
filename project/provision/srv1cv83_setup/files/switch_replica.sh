a48f5aab41c477b39d4f03219932a20b30d85128
dpp <deron73@inbox.ru>
2021-05-14 13:40:51 +0300

    Project: Minor changes

--- project/provision/srv1cv83_setup/files/switch_replica.sh
+++ project/provision/srv1cv83_setup/files/switch_replica.sh
@@ -12,19 +12,16 @@ BASE_UUID=$(/opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID summary lis
 PROC_UUID=$(/opt/1C/v8.3/x86_64/rac connection list --cluster=$CLUSTER_UUID | grep process | head -1 |awk '{print $3}')
 CONN_UUID=$(/opt/1C/v8.3/x86_64/rac connection list --cluster=$CLUSTER_UUID | grep connection | head -1 |awk '{print $3}')
 
-echo $CLUSTER_UUID
-echo $BASE_UUID
-echo $CONN_UUID
-
-#opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID drop --infobase=$BASE_UUID --infobase-user='Администратор' 
-# --infobase-user='Администратор' --dbms=PostgreSQL --db-server=$db_server --db-name=$database_1c --db-user=$db_use --db-pwd=$db_pwd --license-distribution=allow
-
-#/opt/1C/v8.3/x86_64/rac cluster list 
-#/opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID summary list
+#opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID drop --infobase=$BASE_UUID --infobase-user=$infobase_user
 
 /opt/1C/v8.3/x86_64/rac connection --cluster=$CLUSTER_UUID disconnect  --process=$PROC_UUID --connection=$CONN_UUID
 
 /opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID update --infobase=$BASE_UUID --infobase-user=$infobase_user --dbms=PostgreSQL --db-server=$db_server --db-name=$database_1c --db-user=$db_user --db-pwd=$db_pwd --license-distribution=allow
 
-/opt/1C/v8.3/x86_64/rac infobase info --cluster=$CLUSTER_UUID --infobase=$BASE_UUID --infobase-user='Администратор'
+echo $CLUSTER_UUID
+echo $BASE_UUID
+echo $CONN_UUID
+/opt/1C/v8.3/x86_64/rac cluster list 
+/opt/1C/v8.3/x86_64/rac infobase --cluster=$CLUSTER_UUID summary list
+/opt/1C/v8.3/x86_64/rac infobase info --cluster=$CLUSTER_UUID --infobase=$BASE_UUID --infobase-user=$infobase_user
 
